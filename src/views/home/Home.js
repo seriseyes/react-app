5b239ee33e7f5b9299b790295ab2c2e0a2a83b2e
bayarkhuu123 <bayarkhuu.srs@gmail.com>
2022-07-20 17:33:07 +0800

    dynamic image import

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,7 +1,12 @@
 import Loading from "../../components/ui/Loading";
 
 export default function Home() {
+    const images = [
+        "logo.svg",
+    ];
+
     return <>
         <h1>Home Page</h1>
+        {/*{images.map((el, i) => <img width={"100px"} key={i} src={require("../../assets/images/" + el)} alt=""/>)}*/}
     </>;
 }
